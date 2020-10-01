8255f11c6820f2d934ea994e5aee294338e70e98
unknown <divyacharaninabha@gmail.com>
2020-10-01 09:26:18 +0530

    Welcome Message added

---
+++ UserRegistration.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.userRegistration;
+
+public class UserRegistration {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Welcome Message
+		System.out.println("Welcome to User Registration Program");
+
+	}
+
+}
