c8d79247ad70308e36af8bf71c226fff185a2e49
unknown <divyacharaninabha@gmail.com>
2020-10-01 16:20:55 +0530

    Refractored: Refractored for complete attributes validation

--- UserRegistration.java
+++ UserRegistration.java
@@ -5,7 +5,12 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class UserRegistration {
-
+	public static final String FIRST_NAME_PATTERN = "^[A-Z][Aa-zZ]{2,}$";
+	public static final String LAST_NAME_PATTERN = "^[A-Z][Aa-zZ]{2,}$";
+	public static final String EMAIL_PATTERN = "[\\w+_-]+(?:\\.[\\w_-]+)*@(?:[\\w]+\\.)+[a-zA-Z]{2,}";
+	public static final String PHONE_NUMBER_PATTERN = "^[0-9]{2}[\\s][0-9]{10}$";
+	public static final String PASSWORD_PATTERN = "^(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=]).{8,}$";
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		//Welcome Message
@@ -15,32 +20,27 @@ public class UserRegistration {
 		//First Name Validation
 		System.out.println("Enter First Name ");
 		String firstName = sc.nextLine();
-		String pattern = "^[A-Z][Aa-zZ]{2,}$";
-		System.out.println(validate(firstName,pattern)+" first name");
+		System.out.println(validate(firstName,FIRST_NAME_PATTERN)+" first name");
 		
 		//Last Name Validation
 		System.out.println("Enter Last Name ");
 		String lastName = sc.nextLine();
-		pattern = "^[A-Z][Aa-zZ]{2,}$";
-		System.out.println(validate(lastName,pattern)+" last name");
+		System.out.println(validate(lastName,LAST_NAME_PATTERN)+" last name");
 		
 		//Email Validation
 		System.out.println("Enter Email ");
 		String email = sc.nextLine();
-		pattern = "[\\w+_-]+(?:\\.[\\w_-]+)*@(?:[\\w]+\\.)+[a-zA-Z]{2,}";
-		System.out.println(validate(email,pattern)+" email");
+		System.out.println(validate(email,EMAIL_PATTERN)+" email");
 		
 		//Phone Number Validation
 		System.out.println("Enter Phone Number");
 		String phoneNum = sc.nextLine();
-		pattern = "^[0-9]{2}[\\s][0-9]{10}$";
-		System.out.println(validate(phoneNum,pattern)+" phone number");
+		System.out.println(validate(phoneNum,PHONE_NUMBER_PATTERN)+" phone number");
 		
 		//Password Validation
 		System.out.println("Enter Password ");
 		String password = sc.nextLine();
-		pattern = "^(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=]).{8,}$";
-		System.out.println(validate(password,pattern)+" password");
+		System.out.println(validate(password,PASSWORD_PATTERN)+" password");
 
 		sc.close();
 	}
